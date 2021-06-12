d2988df39b039ef404607834fa1d55e94c337699
ExtraE113 <ezra@manmans.com>
2021-06-12 16:43:06 -0700

    inter

--- Death.gd
+++ Death.gd
@@ -1,5 +1,5 @@
 extends Control
-
+signal reset_rng
 onready var score = $VBoxContainer/Score
 
 #func _input(event):
@@ -22,4 +22,10 @@ func toggle_pause():
 
 func _on_Chaser_death():
 	toggle_pause()
-	score.text = "You made it " + str(int(GameState.score/500)) + " meters!"
+	score.text = "You made it " + str(int(GameState.score/500)) + " meters!" \
+	+"\nBest " + str(int(GameState.best_score/500)) + " meters."
+	# todo ask for input, then save
+	# todo show
+	SilentWolf.Scores.persist_score("Ezra", GameState.score)
+	SilentWolf.Scores.get_high_scores()
+	print(SilentWolf.Scores.scores)
--- global.gd
+++ global.gd
@@ -2,3 +2,17 @@ extends Node
 
 
 var score = 0
+var best_score = 0
+
+func _ready():
+	SilentWolf.configure({
+		"api_key": "l2IKmos2jO5M6xQ6iac3s2rHWRc1YuxP94lj39lW",
+		"game_id": "gmtk2021",
+		"game_version": "1.0.0",
+		"log_level": 0
+	})
+
+	SilentWolf.configure_scores({
+		"open_scene_on_close": "res://TitleScreen/TitleScreen.tscn"
+	})
+	
